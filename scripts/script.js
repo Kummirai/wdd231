5708815c01948606e41891c0002d50ccdec63cce
Kummirai <ajaxmilton@hotmail.com>
2025-03-04 22:46:29 +0200

    Add course filtering by category

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ import courses from "./courses.js";
 const menu = document.querySelector(".fa-bars");
 const nav = document.querySelector("nav");
 const subjects = document.querySelector(".subjects");
+const buttons = document.querySelectorAll("button");
 
+//Toggle navbar on smaller screens
 menu.addEventListener("click", ()=>{
     if(nav.classList.contains("none")){
         nav.classList.remove("none");
@@ -12,8 +14,9 @@ menu.addEventListener("click", ()=>{
         nav.classList.remove("show");
         nav.classList.add("none");
     }
-})
+});
 
+//Dynamically add courses mto course section
 let courseSection = "";
 
 const fillCourses = ()=>{
@@ -29,6 +32,32 @@ const fillCourses = ()=>{
 
 fillCourses();
 
+//filter cousrse by category
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+    courseSection = ""
+    if(button.innerText === "ALL"){
+        courses.map((course)=>{
+            courseSection += `
+                <div>
+                    <p>${course.title}</p>
+                </div>
+            `
+        });
+        subjects.innerHTML = courseSection;
+    } else {
+        const selectedCourses = courses.filter((course)=> course.category === button.innerText)
+        selectedCourses.map((course)=>{
+            courseSection += `
+                <div>
+                    <p>${course.title}</p>
+                </div>
+            `
+            });
+        subjects.innerHTML = courseSection; 
+        }     
+    }); 
+});
 
 
 
