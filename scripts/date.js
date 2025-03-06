aa238093108b2d068d5ecd36cdada7ccbdb6c57e
Kummirai <ajaxmilton@hotmail.com>
2025-03-06 13:17:09 +0200

    Include dates in the footer and copyright section.

---
+++ scripts/date.js
@@ -0,0 +1,9 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const currentDate = new Date();
+const thisYear =  currentDate.getFullYear();
+
+currentYear.innerHTML = thisYear;
+lastModified.innerHTML = currentDate
+
