ca4dfae5b2aa9a6dff4c4e6b6ef8ccada3906aff
Kummirai <ajaxmilton@hotmail.com>
2025-03-08 15:36:09 +0200

    Add a pseudo-element for an arrow.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,13 @@
 const menu = document.querySelector(".fa-bars");
 const nav = document.querySelector("nav");
+const navAchorTags = document.querySelectorAll(".navigation");
+
+
+navAchorTags.forEach((anchor)=>{
+        if(anchor.href === window.location.href){
+            anchor.classList.add("active");
+        }
+});
 
 //Toggle navbar on smaller screens
 menu.addEventListener("click", ()=>{
